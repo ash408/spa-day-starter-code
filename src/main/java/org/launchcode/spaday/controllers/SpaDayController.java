8c4cd9de1303272bd0b635427ec984953d19dca0
JaneDoe <janedoe@notanemail.com>
2023-04-27 19:05:51 -0500

    Finished studio

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -63,6 +63,12 @@ public class SpaDayController {
             }
         }
 
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+
+        model.addAttribute("facials", appropriateFacials);
+
         return "menu";
     }
 }
